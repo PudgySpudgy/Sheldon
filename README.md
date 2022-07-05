1b53ccab67499f1686ca4b8439f0f97c05512191
NotSoPudgySpudgy <phillips0517@protonmail.com>
2022-07-05 16:16:58 -0400

    Functional basis of RPS committed to pull from if necessary in future. if else chains were the correct call.

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Well, at least the first three. I'll try to incorporate the rest as I learn.
 
 Initial creation of index.html with basic boilerplate verbiage.
 Initial creation of index.js.
+
+7/5/2022 Changes
+:: WIP code from previous week deleted as it became apparent that nested if loops were not the right path.
+Attempts to debut were fruitless, and discovered that the code was being read w/o curly braces, despite being necessary. Constructed a workable program, one line at a time. And the commit on 7/5/2022 appears fully functional for the Rock Paper Scissors TOP request.
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function computerPlay() {
 //   This creates an array of the options for the game.
-    var computerOptions = ["rock", "paper", "scissors", "lizard", "Spock"];
+    var computerOptions = ["rock", "paper", "scissors"]; // add "lizard", and "Spock" at a later date
 //    I am asking for a new variable to be randomly generated out of the listed array. Additionally, I am adding a math.floor component(to round down) and a *length designation to limit based on the amount of items in the array.
     var computerRandom = computerOptions[Math.floor(Math.random()*computerOptions.length)];
 // This is storing the results of the random var as "computerPlay" to be called by the main program
@@ -10,57 +10,48 @@ function computerPlay() {
 // computerPlay();
 // This has been commented out to see if it is necessary.
 
-function gameRound () {
-    let playerSelection = computerPlay();
-    console.log(playerSelection);
-    let computerSelection = computerPlay();
-    console.log(computerSelection);
-
-    if (playerSelection === computerSelection) {
-        console.log("It's a tie!")
-    }
-    // while (playerSelection !== computerSelection) 
-    //     if ((playerSelection === "rock") && (computerSelection === "paper")){
-    //     console.log("You lose! Paper covers rock!");
-    //     break;
-    // } if ((playerSelection === "rock") && (computerSelection === "scissors")){
-    //     console.log("You win! Rock smashes scissors!");
-    //     // break;
-    // } if ((playerSelection === "rock") && (computerSelection === "lizard")){
-    //     console.log("You win! Rock crushes lizard!");
-    //     // break;
-    // } if ((playerSelection === "rock") && (computerSelection === "Spock")){
-    //     console.log("You lose! Spock vaporizes rock!");
-    //     // break;
-    // } if ((playerSelection === "paper") && (computerSelection === "rock")){
-    //     console.log("You win! Paper covers rock!");
-    //     // break;
-    // } if ((playerSelection === "paper") && (computerSelection === "scissors")){
-    //     console.log("You lose! Scissors cut paper!");
-    //     // break;
-    // } if ((playerSelection === "paper") && (computerSelection === "lizard")){
-    //     console.log("You lose! Lizard eats paper!");
-    //     // break;
-    // } if ((playerSelection === "paper") && (computerSelection === "Spock")){
-    //     console.log("you win! Paper disproves Spock!");
-    //     // break;
-    } 
-
-// I think I need to evaluate the type of loop I need to be using to complete this program. Nested 'if' statements seem to break the code. Is this because I led with a 'while' statement? I have commented out all of the offending code, and will reevaluate in the morning.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function gameRound () {
     
- gameRound();
+//     let playerSelection = computerPlay();
+//     console.log(playerSelection);
+//     let computerSelection = computerPlay();
+//     console.log(computerSelection);
+
+//         if (playerSelection === computerSelection){
+//             console.log("It`s a tie!");
+//         } else {
+//             console.log('Who will win?');
+//     };
+// }
+// gameRound();
+
+// Above is workable code that only has a simple T/F check. Commented out to proceed with the entirety of the problem.
+
+function gameRound () {
+    
+    let playerSelection = computerPlay();
+    // console.log(playerSelection);
+    let computerSelection = computerPlay();
+    // console.log(computerSelection);
+
+        if (playerSelection === computerSelection){
+            console.log("It`s a tie!");
+        } else if  ((playerSelection == 'rock') && (computerSelection === 'paper')) {
+            console.log('You lose. Paper covers rock!');
+        } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+            console.log('You win. Rock smashes scissors!');
+        } else if ((playerSelection === 'paper') && (computerSelection === 'rock')){
+            console.log('You win. Paper covers rock!');
+        } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')){
+            console.log('You lose. Scissors cut paper!');
+        } else if ((playerSelection === 'scissors') && (computerSelection === 'rock')){
+            console.log('Your lose. Rock smashes scissors!');
+        } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')){
+            console.log('You win. Scissors cut paper!');
+    
+        }
+    
+        
+
+    }
+gameRound();
